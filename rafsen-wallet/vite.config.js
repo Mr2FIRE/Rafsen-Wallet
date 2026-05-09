aabd9fae18b0e0214b011069c91c60f9b797d41f
Mr2FIRE <mostafafire127@gmail.com>
2026-05-09 21:14:46 +0300

    fix: externalize fsevents

--- rafsen-wallet/vite.config.js
+++ rafsen-wallet/vite.config.js
@@ -1,14 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { nodePolyfills } from 'vite-plugin-node-polyfills'
-import topLevelAwait from "vite-plugin-top-level-await"
-import wasm from "vite-plugin-wasm"
 
 export default defineConfig({
   plugins: [
     react(),
-    wasm(),
-    topLevelAwait(),
     nodePolyfills({
       include: ['buffer', 'process', 'crypto', 'stream', 'util', 'events'],
       globals: {
@@ -20,9 +16,8 @@ export default defineConfig({
   ],
   build: {
     target: 'esnext',
-    assetsInlineLimit: 0,
+    rollupOptions: {
+      external: ['fsevents'],
+    },
   },
-  optimizeDeps: {
-    exclude: ['tiny-secp256k1', '@bitcoinerlab/secp256k1']
-  }
 })
