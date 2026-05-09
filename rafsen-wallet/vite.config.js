b193532c39f210644763c41a9801db39f3f367e9
Mr2FIRE <mostafafire127@gmail.com>
2026-05-09 20:58:09 +0300

    fix: remove merge conflicts from vite.config.js

--- rafsen-wallet/vite.config.js
+++ rafsen-wallet/vite.config.js
@@ -1,24 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { nodePolyfills } from 'vite-plugin-node-polyfills'
-<<<<<<< HEAD
 import topLevelAwait from "vite-plugin-top-level-await"
-import wasm from "vite-plugin-wasm" // Added this
-=======
->>>>>>> 99abe21d59876c63d44da7729f354b6d0c1fda35
+import wasm from "vite-plugin-wasm"
 
 export default defineConfig({
   plugins: [
     react(),
-<<<<<<< HEAD
-    wasm(), // Added this
+    wasm(),
     topLevelAwait(),
     nodePolyfills({
-=======
-    // Ripped out the crashing top-level-await plugin!
-    nodePolyfills({
       include: ['buffer', 'process', 'crypto', 'stream', 'util', 'events'],
->>>>>>> 99abe21d59876c63d44da7729f354b6d0c1fda35
       globals: {
         Buffer: true,
         global: true,
@@ -28,19 +20,9 @@ export default defineConfig({
   ],
   build: {
     target: 'esnext',
-<<<<<<< HEAD
-    // This tells Vite to treat WASM as a constant asset
-    assetsInlineLimit: 0, 
+    assetsInlineLimit: 0,
   },
   optimizeDeps: {
-    // This prevents Vite from trying to "pre-bundle" the problematic library
-    exclude: ['tiny-secp256k1']
+    exclude: ['tiny-secp256k1', '@bitcoinerlab/secp256k1']
   }
 })
-=======
-  },
-  optimizeDeps: {
-    exclude: ['@bitcoinerlab/secp256k1']
-  }
-})
->>>>>>> 99abe21d59876c63d44da7729f354b6d0c1fda35
